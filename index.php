c737a89c3dfcf8f1094f3144a2b92b0ccff42953
Manuel <manuelloverso60@gmail.com>
2024-04-18 13:51:15 +0200

    add a production instance

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
+include_once __DIR__ . '/Models/Production.php';
 
+$productions = [new Production('Lord of the Rings', 'english', 9) ];
+var_dump($productions);
 
+echo $productions[0]->language;
 
 ?>
 
